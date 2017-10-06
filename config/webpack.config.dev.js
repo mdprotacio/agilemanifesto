52feac68b9e5c475d04e71a48fc9ca2d2bdbd6f2
Rupert Rutland <rupertrutland@gmail.com>
2017-10-06 23:18:27 +0200

    update webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -69,6 +69,7 @@ module.exports = {
       path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
   },
   resolve: {
+    mainFiles: ['index'],
     // This allows you to set a fallback for where Webpack should look for modules.
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
