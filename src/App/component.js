7e0508acbbf67d5716c26f4363a3c1935ec94846
Rupert Rutland <rupertrutland@gmail.com>
2017-10-07 09:03:13 +0200

    add homepage content

--- src/App/component.js
+++ src/App/component.js
@@ -9,13 +9,33 @@ export class component extends Component {
         <header className={styles.AppHeader}>
           <img src={logo} className={styles.AppLogo} alt="logo" />
           <h1 className={styles.AppTitle}>
-            Welcome to the Agile Manifesto (in React)
+            Manifesto for Agile Software Development
           </h1>
         </header>
-        <p className={styles.AppIntro}>
-          To get started, edit <code>src/App/component.js</code> and save to
-          reload.
-        </p>
+        <div className={styles.AppIntro}>
+          <p>
+            We are uncovering better ways of developing<br />
+            software by doing it and helping others do it.<br />
+            Through this work we have come to value:<br />
+          </p>
+
+          <p>
+            Individuals and interactions over processes and tools<br />
+            Working software over comprehensive documentation<br />
+            Customer collaboration over contract negotiation<br />
+            Responding to change over following a plan<br />
+          </p>
+
+          <p>
+            That is, while there is value in the items on<br />
+            the right, we value the items on the left more.<br />
+          </p>
+        </div>
+        <div>
+          &copy; 2001, the above authors<br />
+          this declaration may be freely copied in any form, <br />
+          but only in its entirety through this notice.
+        </div>
       </div>
     );
   }
