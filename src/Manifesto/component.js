ba9db8867db691be0801b5d7c9f0817beaf55a12
Rupert Rutland <rupertrutland@gmail.com>
2017-10-13 12:02:25 +0200

    update markup

--- src/Manifesto/component.js
+++ src/Manifesto/component.js
@@ -7,60 +7,68 @@ import styles from './styles.scss';
 export class component extends Component {
   render() {
     return (
-      <div
+      <article
         className={styles.manifesto}
         style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}
       >
         <header>
           <h1>Manifesto for Agile Software Development</h1>
         </header>
-        <div>
+        <section>
           <p>
             We are uncovering better ways of developing<br />
             software by doing it and helping others do it.<br />
             Through this work we have come to value:<br />
           </p>
 
-          <p>
-            <strong>Individuals and interactions</strong> over processes and
-            tools<br />
-            <strong>Working software</strong> over comprehensive documentation<br />
-            <strong>Customer collaboration</strong> over contract negotiation<br />
-            <strong>Responding to change</strong> over following a plan<br />
-          </p>
+          <ul>
+            <li>
+              <strong>Individuals and interactions</strong> over processes and
+              tools
+            </li>
+            <li>
+              <strong>Working software</strong> over comprehensive documentation<br />
+            </li>
+            <li>
+              <strong>Customer collaboration</strong> over contract negotiation<br />
+            </li>
+            <li>
+              <strong>Responding to change</strong> over following a plan<br />
+            </li>
+          </ul>
 
           <p>
             That is, while there is value in the items on<br />
             the right, we value the items on the left more.<br />
           </p>
-        </div>
-        <div>
-          <p>
-            Kent Beck<br />
-            Mike Beedle<br />
-            Arie van Bennekum<br />
-            Alistair Cockburn<br />
-            Ward Cunningham<br />
-            Martin Fowler<br />
-            James Grenning<br />
-            Jim Highsmith<br />
-            Andrew Hunt<br />
-            Ron Jeffries<br />
-            Jon Kern<br />
-            Brian Marick<br />
-            Robert C. Martin<br />
-            Steve Mellor<br />
-            Ken Schwaber<br />
-            Jeff Sutherland<br />
-            Dave Thomas<br />
-          </p>
-        </div>
-        <div>
+        </section>
+        <section className="authors">
+          <ul>
+            <li>Kent Beck</li>
+            <li>Mike Beedle</li>
+            <li>Arie van Bennekum</li>
+            <li>Alistair Cockburn</li>
+            <li>Ward Cunningham</li>
+            <li>Martin Fowler</li>
+            <li>James Grenning</li>
+            <li>Jim Highsmith</li>
+            <li>Andrew Hunt</li>
+            <li>Ron Jeffries</li>
+            <li>Jon Kern</li>
+            <li>Brian Marick</li>
+            <li>Robert C. Martin</li>
+            <li>Steve Mellor</li>
+            <li>Ken Schwaber</li>
+            <li>Jeff Sutherland</li>
+            <li>Dave Thomas</li>
+          </ul>
+        </section>
+        <small>
           &copy; 2001, the above authors<br />
           this declaration may be freely copied in any form, <br />
           but only in its entirety through this notice.
-        </div>
-      </div>
+        </small>
+      </article>
     );
   }
 }
